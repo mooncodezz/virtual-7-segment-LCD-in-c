14a83765f4d1de02f8256fc2bf061b9eab04b734
Naruesorn Krabkhuntod <92029549+mooncodezz@users.noreply.github.com>
2024-02-03 14:45:40 +0700

    Update and rename app.c to 10 line Funny Version.c

--- 10 line Funny Version.c
+++ 10 line Funny Version.c
@@ -7,5 +7,4 @@ void main () {
     for(int j = 0; j < s - '0'; j++) for(int i = 0; i < strlen(n); i++) printf("%s%.*s%s%s", (strchr("045689", n[i])? "*" : " "), s - '0', off, (strchr("56", n[i])? " " : "*"), ((i == strlen(n) - 1)? "\n":" "));
     for(int i = 0; i < strlen(n); i++) strchr("017", n[i])? printf(" %.*s %s", s - '0', off, ((i == strlen(n) - 1)? "\n" : " ")) : printf(" %.*s %s", s - '0', on, ((i == strlen(n) - 1)? "\n" : " "));
     for(int j = 0; j < s - '0'; j++) for(int i = 0; i < strlen(n); i++) printf("%s%.*s%s%s", (strchr("0268", n[i])? "*" : " "), s - '0', off, (strchr("2", n[i])? " " : "*"), ((i == strlen(n) - 1)? "\n" : " "));
-    for(int i = 0; i < strlen(n); i++) strchr("147", n[i])? printf(" %.*s %s", s - '0', off, ((i == strlen(n) - 1)? "\n" : " ")) : printf(" %.*s %s", s - '0', on, ((i == strlen(n) - 1)? "\n" : " "));
-}
+    for(int i = 0; i < strlen(n); i++) strchr("147", n[i])? printf(" %.*s %s", s - '0', off, ((i == strlen(n) - 1)? "\n" : " ")) : printf(" %.*s %s", s - '0', on, ((i == strlen(n) - 1)? "\n" : " ")); }
